f4eb769119102fbd32f85974c8514e1a1f06c8f3
FeasterS <Seth.Feaster@gmail.com>
2019-08-19 23:30:31 -0400

    Cleaning for posterity and sharing
    Confirmation of credentials is made to a function. Passing by reference didn't seem to work, so returning the value is the best bet.
    Additional comments made including links to references used.
    Genericised for future reference.

--- Quickstart.py
+++ Quickstart.py
@@ -1,3 +1,11 @@
+# Initial setup for allowing authorization is here:
+#     https://developers.google.com/sheets/api/quickstart/python
+#
+# If you already have your authorization files, remember to run this:
+#     pip install --upgrade google-api-python-client
+#                           google-auth-httplib2
+#                           google-auth-oauthlib
+
 from __future__ import print_function
 import datetime
 import random
@@ -8,54 +16,62 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
+# Note: This scope is read/write. See other scopes here:
+#     https://developers.google.com/sheets/api/guides/authorizing
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # The ID and range of a sample spreadsheet.
+# These must be changed for your application
 SPREADSHEET_ID    = '1WS0uvF-a3Ui33ejOuZlW0wTZSjfSaXbqg3Hq_xjRuSM'
 SPREADSHEET_RANGE = "Python!A:B"
 
-def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    creds = None
+def confirm_credentials( my_creds ):
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
+            my_creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+    if not my_creds or not my_creds.valid:
+        if my_creds and my_creds.expired and my_creds.refresh_token:
+            my_creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            my_creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
+    return my_creds
 
+def main():
+    creds = None # default to no credentials, let them be loaded
+    creds = confirm_credentials( creds )
+    # Get the sercice connection
     service = build('sheets', 'v4', credentials=creds)
+    # Infinite loop to continuously run, as application will in the future.
+    # There is a break condition for "quit".
+    while ( True ):
+        in       = input( "Awaiting Input\n") # Holds until Input is entered
+        time_now = datetime.datetime.now( )   # Grab time for timestamp
 
-    time_now = datetime.datetime.now( )
-    id       = input( )
-
-    resource = {
-        "majorDimension": "ROWS",
-        "values": [[id, str( time_now )]],
-    }
-
-    # Call the Sheets API
-    request = service.spreadsheets( ).values( ).append(
-       spreadsheetId=SPREADSHEET_ID,
-       range=SPREADSHEET_RANGE,
-       body=resource,
-       valueInputOption="RAW", )
-    response = request.execute( )
-
-    print( "Complete!" )
+        if ( in.lower( )=="quit" ):
+            break
+        # Builds up the resource that's going to be uploaded
+        resource = {
+            "majorDimension": "ROWS",          # Each main box will be one of these
+            "values": [[in, str( time_now )]], # One row, two values
+        }
+        # Prepare a request
+        request = service.spreadsheets( ).values( ).append(
+            spreadsheetId=SPREADSHEET_ID,
+            range=SPREADSHEET_RANGE,
+            body=resource,
+            valueInputOption="RAW", )
+        # Get the response from the execution. This isn't handled, but assumed successful.
+        response = request.execute( )
+        print( "Uploaded!" )
 
 if __name__ == '__main__':
     main()
